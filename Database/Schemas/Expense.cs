9e7fd93bb928828e8ef9a1feb4125aeb6799814e
TheAncientOwl <dele.alex.o.o@gmail.com>
2023-03-30 20:35:05 +0300

    implemented LoadFromId, Save, Update, Delete, Equals, GetHashCode

--- Database/Schemas/Expense.cs
+++ Database/Schemas/Expense.cs
@@ -1,17 +1,10 @@
 ﻿using Microsoft.Office.Interop.Access.Dao;
 using System;
+using System.Collections.Generic;
 
 namespace BudgetDjinni.Database.Schemas
 {
-//ID  int
-//Denumire    string
-//Valoare float
-//Data    DateTime
-//Categorie   CategorieCheltuieli
-//Frecvență   FrecvențăCheltuieli
-//Detalii string
-
-    public class Expense
+    public class Expense : IDatabaseObject, IEquatable<Expense>
     {
         #region Database Table Definitions
         public static readonly string TableName = "Expenses";
@@ -93,6 +86,133 @@ namespace BudgetDjinni.Database.Schemas
 
             Manager.DbInstance.Relations.Append(frequencyRelation);
         }
+
         #endregion Database Table Definitions
+
+        #region Properties
+        public int Id { get; private set; }
+        public string Name { get; set; }
+        public double Value { get; set; }
+        public DateTime Date { get; set; }
+        public ExpenseCategory Category { get; set; }
+        public ExpenseFrequency Frequency { get; set; }
+        #endregion Properties
+
+        #region Constructors
+        public Expense(string name, double value, DateTime date, int categoryId, int frequencyId)
+        {
+            Id = -1;
+
+            Name = name;
+            Value = value;
+            Date = date;
+
+            Category = categoryId == -1 ? null : new ExpenseCategory(categoryId);
+            Frequency = frequencyId == -1 ? null : new ExpenseFrequency(frequencyId);
+        }
+
+        public Expense() : this("Empty Expense", 0, DateTime.Now, -1, -1) { }
+
+        public Expense(int id)
+        {
+            LoadFromId(id);
+        }
+        #endregion Constructors
+
+        #region Public API
+        public void LoadFromId(int id)
+        {
+            Recordset rs = Manager.DbInstance.OpenRecordset(TableName, RecordsetTypeEnum.dbOpenDynaset);
+            rs.FindFirst(Fields.ID + " = " + id);
+
+            if (rs.NoMatch)
+            {
+                rs.Close();
+                throw new Exception("No Expense with ID " + id + " matched");
+            }
+
+            Id = (int)rs.Fields[Fields.ID].Value;
+            Name = (string)rs.Fields[Fields.Name].Value;
+            Value = (double)rs.Fields[Fields.Value].Value;
+            Date = (DateTime)rs.Fields[Fields.Date].Value;
+            Category = new ExpenseCategory((int)rs.Fields[Fields.CategoryId].Value);
+            Frequency = new ExpenseFrequency((int)rs.Fields[Fields.FrequencyId].Value);
+
+            rs.Close();
+        }
+
+        public void Save()
+        {
+            Recordset rs = Manager.DbInstance.OpenRecordset(TableName, RecordsetTypeEnum.dbOpenDynaset);
+
+            rs.AddNew();
+            Id = (int)rs.Fields[Fields.ID].Value;
+
+            rs.Fields[Fields.Name].Value = Name;
+            rs.Fields[Fields.Value].Value = Value;
+            rs.Fields[Fields.Date].Value = Date;
+            rs.Fields[Fields.CategoryId].Value = Category.Id;
+            rs.Fields[Fields.FrequencyId].Value = Frequency.Id;
+
+            rs.Update();
+            rs.Close();
+        }
+
+        public void Update()
+        {
+            Recordset rs = Manager.DbInstance.OpenRecordset(TableName, RecordsetTypeEnum.dbOpenDynaset);
+            rs.FindFirst(Fields.ID + " = " + Id);
+
+            if (rs.NoMatch)
+            {
+                rs.Close();
+                throw new Exception("No Expense with ID " + Id + " matched");
+            }
+
+            rs.Edit();
+            rs.Fields[Fields.Name].Value = Name;
+            rs.Fields[Fields.Value].Value = Value;
+            rs.Fields[Fields.Date].Value = Date;
+            rs.Fields[Fields.CategoryId].Value = Category.Id;
+            rs.Fields[Fields.FrequencyId].Value = Frequency.Id;
+
+            rs.Update();
+            rs.Close();
+        }
+
+        public void Delete()
+        {
+            Recordset rs = Manager.DbInstance.OpenRecordset(TableName, RecordsetTypeEnum.dbOpenDynaset);
+            rs.FindFirst(Fields.ID + " = " + Id);
+
+            if (rs.NoMatch)
+            {
+                rs.Close();
+                throw new Exception("No Expense with ID " + Id + " matched");
+            }
+
+            rs.Delete();
+
+            rs.Close();
+        }
+
+        public bool Equals(Expense other) =>
+            (Id == other.Id) && (Name == other.Name) && (Value == other.Value) && 
+            (Date.Day == other.Date.Day) && (Date.Month == other.Date.Month) && (Date.Year == other.Date.Year) &&
+            (Category.Equals(other.Category)) && (Frequency.Equals(other.Frequency));
+        public override bool Equals(object obj) => Equals(obj as Expense);
+
+        public override int GetHashCode()
+        {
+            int hashCode = 1342306499;
+            hashCode = hashCode * -1521134295 + Id.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Name);
+            hashCode = hashCode * -1521134295 + Value.GetHashCode();
+            hashCode = hashCode * -1521134295 + Date.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<ExpenseCategory>.Default.GetHashCode(Category);
+            hashCode = hashCode * -1521134295 + EqualityComparer<ExpenseFrequency>.Default.GetHashCode(Frequency);
+            return hashCode;
+        }
+        #endregion Public API
     }
 }
