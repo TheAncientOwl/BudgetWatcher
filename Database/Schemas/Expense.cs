7e5f8350563e4afff37f3b2ab7b0ee4e6b8f83aa
TheAncientOwl <dele.alex.o.o@gmail.com>
2023-03-31 12:38:55 +0300

    added details as property

--- Database/Schemas/Expense.cs
+++ Database/Schemas/Expense.cs
@@ -94,24 +94,26 @@ namespace BudgetWatcher.Database.Schemas
         public string Name { get; set; }
         public double Value { get; set; }
         public DateTime Date { get; set; }
+        public string Details { get; set; }
         public ExpenseCategory Category { get; set; }
         public ExpenseFrequency Frequency { get; set; }
         #endregion Properties
 
         #region Constructors
-        public Expense(string name, double value, DateTime date, int categoryId, int frequencyId)
+        public Expense(string name, double value, DateTime date, string details, int categoryId, int frequencyId)
         {
             Id = -1;
 
             Name = name;
             Value = value;
             Date = date;
+            Details = details;
 
             Category = categoryId == -1 ? null : new ExpenseCategory(categoryId);
             Frequency = frequencyId == -1 ? null : new ExpenseFrequency(frequencyId);
         }
 
-        public Expense() : this("Empty Expense", 0, DateTime.Now, -1, -1) { }
+        public Expense() : this("Empty Expense", 0, DateTime.Now, "-", -1, -1) { }
 
         public Expense(int id)
         {
@@ -128,6 +130,7 @@ namespace BudgetWatcher.Database.Schemas
             Name = (string)rs.Fields[Fields.Name].Value;
             Value = (double)rs.Fields[Fields.Value].Value;
             Date = (DateTime)rs.Fields[Fields.Date].Value;
+            Details = (string)rs.Fields[Fields.Details].Value;
             Category = new ExpenseCategory((int)rs.Fields[Fields.CategoryId].Value);
             Frequency = new ExpenseFrequency((int)rs.Fields[Fields.FrequencyId].Value);
 
@@ -143,6 +146,7 @@ namespace BudgetWatcher.Database.Schemas
             rs.Fields[Fields.Name].Value = Name;
             rs.Fields[Fields.Value].Value = Value;
             rs.Fields[Fields.Date].Value = Date;
+            rs.Fields[Fields.Details].Value = Details;
             rs.Fields[Fields.CategoryId].Value = Category.Id;
             rs.Fields[Fields.FrequencyId].Value = Frequency.Id;
 
@@ -158,6 +162,7 @@ namespace BudgetWatcher.Database.Schemas
             rs.Fields[Fields.Name].Value = Name;
             rs.Fields[Fields.Value].Value = Value;
             rs.Fields[Fields.Date].Value = Date;
+            rs.Fields[Fields.Details].Value = Details;
             rs.Fields[Fields.CategoryId].Value = Category.Id;
             rs.Fields[Fields.FrequencyId].Value = Frequency.Id;
 
@@ -175,7 +180,7 @@ namespace BudgetWatcher.Database.Schemas
         }
 
         public bool Equals(Expense other) =>
-            (Id == other.Id) && (Name == other.Name) && (Value == other.Value) && 
+            (Id == other.Id) && (Name == other.Name) && (Value == other.Value) && (Details == other.Details) &&
             (Date.Day == other.Date.Day) && (Date.Month == other.Date.Month) && (Date.Year == other.Date.Year) &&
             (Category.Equals(other.Category)) && (Frequency.Equals(other.Frequency));
         public override bool Equals(object obj) => Equals(obj as Expense);
@@ -187,6 +192,7 @@ namespace BudgetWatcher.Database.Schemas
             hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Name);
             hashCode = hashCode * -1521134295 + Value.GetHashCode();
             hashCode = hashCode * -1521134295 + Date.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Details);
             hashCode = hashCode * -1521134295 + EqualityComparer<ExpenseCategory>.Default.GetHashCode(Category);
             hashCode = hashCode * -1521134295 + EqualityComparer<ExpenseFrequency>.Default.GetHashCode(Frequency);
             return hashCode;
